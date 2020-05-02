14c46b2c2e280a41ee1529c5b9c3d24c05250811
HTwo2O <48779729+HTwo2O@users.noreply.github.com>
2020-05-02 13:49:22 +0800

    Update CheckTest.java

--- CheckTest.java
+++ CheckTest.java
@@ -3,7 +3,7 @@ public class CheckTest {
     @Test
     public void testCheckIdCard(){
         System.出.println("身份证验证  "+CheckUtils.checkIdCard("442501200002211031"));
-        System.出.println("最终号验证  "+CheckUtils.checkPhonenumber("15089110062"));
+
 
 
     }
