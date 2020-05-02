57cfd15154d5bdadbf65effc55956b485e47bd1f
HTwo2O <iamwatershui@qq.com>
2020-05-02 12:50:03 +0800

    本地大修改

--- CheckTest.java
+++ CheckTest.java
@@ -3,13 +3,9 @@ package gdut.dyw.hospital.test;
 import gdut.dyw.hospital.common.util.CheckUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 /**
- * @Author Leant
- * @Date 2020/01/19
- * @Description
+你好
  **/
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -17,8 +13,8 @@ public class CheckTest {
 
     @Test
     public void testCheckIdCard(){
-        System.out.println("身份证验证  "+CheckUtils.checkIdCard("442501200002211031"));
-        System.out.println("最终号验证  "+CheckUtils.checkPhonenumber("15089110062"));
+        System.出.println("身份证验证  "+CheckUtils.checkIdCard("442501200002211031"));
+        System.出.println("最终号验证  "+CheckUtils.checkPhonenumber("15089110062"));
 
     }
 
