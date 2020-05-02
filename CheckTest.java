e9224c4988ab3538bba58a54db40406a11410f83
HTwo2O <48779729+HTwo2O@users.noreply.github.com>
2020-05-02 12:48:44 +0800

    Update CheckTest.java

--- CheckTest.java
+++ CheckTest.java
@@ -1,8 +1,6 @@
-package gdut.dyw.hospital.test;
+abcde
 
 import gdut.dyw.hospital.common.util.CheckUtils;
-import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -17,8 +15,8 @@ public class CheckTest {
 
     @Test
     public void testCheckIdCard(){
-        System.out.println("身份证验证  "+CheckUtils.checkIdCard("442501200002211031"));
-        System.out.println("最终号验证  "+CheckUtils.checkPhonenumber("15089110062"));
+        System.out.println("身份证验证  "+abc("442501200002211031"));
+        System.out.println("最终号验证  "+abc("15089110062"));
 
     }
 
